253c9da8b176c13f639a0b28f2b26ac51bd319f1
Jack Ritchie <jwf.ritchie@gmail.com>
2021-03-19 16:50:33 +1100

    first local to server push, HTML built and styled with css, will continue to build the java functionality

---
+++ java.js
@@ -0,0 +1,12 @@
+const startButton = document.getElementById("startbutton");
+const startPage = document.getElementById("startPage");
+const stepOne = document.getElementById("stepOne");
+const stepOne = document.getElementById("stepTwo");
+const stepOne = document.getElementById("stepThree");
+const stepOne = document.getElementById("stepFour");
+const stepOne = document.getElementById("stepFive");
+
+function startQuiz(){
+  stepOne.style.display = "flex";
+  startPage.style.display = "none";
+}
